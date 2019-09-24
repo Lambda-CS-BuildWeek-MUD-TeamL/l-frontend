906841f76cf9b1b9e35f6a71d577e3577498ca80
chrispetito <christopherpetito@gmail.com>
2019-09-24 13:20:16 -0700

    game skeleton layout

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,18 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export default class HomePage extends React.Component {
-    constructor() {
-        super()
-        this.state = {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-        }
-    }
-
-    render() {
-        return(
-            <div>
-                <h1>Home Page</h1>
+  render() {
+    return (
+      <div className="homepage-div">
+        <h1 className="homepage-header">Team L - MUD</h1>
+        <div className="game-div"></div>
+        <div className="controls-div">
+          <div className="sub-control">
+            <div className="button-div">
+              <button className="ind-button"><i class="fas fa-arrow-up"></i></button>
             </div>
-        )
-    }
+            <div className="button-div">
+              <button className="ind-button"><i class="fas fa-arrow-left"></i></button>
+              <button className="ind-button"><i class="fas fa-arrow-right"></i></button>
+            </div>
+            <div className="button-div">
+              <button className="ind-button"><i class="fas fa-arrow-down"></i></button>
+            </div>
+          </div>
+          <div className="sub-control"></div>
+        </div>
+      </div>
+    );
+  }
 }
