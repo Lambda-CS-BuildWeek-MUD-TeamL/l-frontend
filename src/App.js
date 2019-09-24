1c6622c814fbca2ef91166854afad7e8db4c8c47
chrispetito <christopherpetito@gmail.com>
2019-09-24 12:15:35 -0700

    add PrivateRoute

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
-import Login from './components'
-import SignUp from './components'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
 import './App.css';
 
 function App() {
@@ -9,14 +9,13 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             CS21 Build Week 1 - MUD - Team L
           </p>
         </header>
         <>
-        <Route exact path={} component={Login} />
-        <Route exact path={} component={SignUp} />
+        <Route exact path={'/login'} component={Login} />
+        <Route exact path={'/signup'} component={SignUp} />
         
         </>
       </div>
